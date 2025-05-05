054767b7686113cb0c7b14e12cd957a489a18a27
re-verse <conolly@gmail.com>
2025-05-05 10:55:26 -0500

    Update cross_sub_ban_bot.py

--- cross_sub_ban_bot.py
+++ cross_sub_ban_bot.py
@@ -285,9 +285,14 @@ def sync_bans_from_sub(sub):
             if user.lower() in EXEMPT_USERS or is_mod(sr, user):
                 continue
 
-            if any(r.get('Username', '').lower() == user.lower() for r in SHEET_CACHE):
-                print(f"[SKIP] User {user} already logged in sheet. Skipping.")
-                continue
+			if any(
+				r.get('Username', '').lower() == user.lower() and
+				r.get('SourceSub', '').lower() == source.lower()
+				for r in SHEET_CACHE
+			):
+				print(f"[SKIP] User {user} already logged in sheet from {source}. Skipping.")
+				continue
+
 
             if already_logged_action(log_id):
                 continue
